a3902e9d50e10a9e27cbb6275ef9c39ef392eed3
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-06-24 17:24:03 +0200

    [UPD] variable's declaration and initialization, used .getElementById.innerHTML to wrote password into document

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 let first_name = prompt('Inserisci il tuo nome')
 let last_name = prompt('Inserisci il tuo cognome')
 let favorite_color = prompt('Inserisci il tuo colore preferito')
+
+let password_generated = `${first_name}${last_name}${favorite_color}23`
+
+console.log(password_generated)
+document.getElementById('password_generated').innerHTML = password_generated
